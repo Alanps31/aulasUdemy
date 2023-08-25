6d2cd32485463f3ce4a70a1c65522700766ead2c
Alan <alan.pinheirodossantos@gmail.com>
2023-08-25 11:13:12 -0300

    lógicos

---
+++ src/main/kotlin/operadores/logicos.kt
@@ -0,0 +1,11 @@
+package operadores
+
+fun main() {
+    val trabalho1: Boolean = true
+    val trabalho2: Boolean = false
+
+    println(trabalho1 || trabalho2)
+    println(trabalho1 && trabalho2)
+    println(trabalho1 xor trabalho2)
+
+}
