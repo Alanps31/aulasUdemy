207b69f2010270ee564ec9a35f124db8a46c4c78
Alan <alan.pinheirodossantos@gmail.com>
2023-08-29 19:13:50 -0300

    Classes #1

--- src/main/kotlin/classes/classeBasica.kt
+++ src/main/kotlin/classes/classeBasica.kt
@@ -1,1 +1,11 @@
 package classes
+class Cliente () {
+    var nome = "Joao"
+}
+
+fun main() {
+    val cliente = Cliente()
+    println(cliente.nome)
+    cliente.nome = "Carlos Alberto"
+    println(cliente.nome)
+}
